3f3d11f142b0c5c9d735b7641acbc0dd8365206f
aplsek <aplsek@gmail.com>
2014-02-04 21:29:10 -0800

    python test

--- src/main/python/hw4-1.py
+++ src/main/python/hw4-1.py
@@ -6,4 +6,8 @@ connection = pymongo.Connection("mongodb://localhost", safe=True)
 db = connection.products
 foo = db.students
 
+doc = foo.find_one({'student_id':1})
+print "first :" , doc['student_id'], " is ", doc['scores']
+
+
 
